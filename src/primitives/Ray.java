53705bcd22c7bc4bce9f551e021363b317fda0f0
אליסף דימנט <edimant@g.jct.ac.il>
2021-03-07 22:12:09 +0200

    EX_1 Final Commit

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,16 +1,22 @@
 package primitives;
-
+/**
+ * The set of points on a straight line that are on one side relative to a given point
+ */
 public class Ray {
     /**
-     *
-     */
+     * The point from which the ray start
+      */
     private Point3D p0;
     /**
-     *
+     * The unit vector that emerges from the point
      */
     private Vector dir;
 
-
+    /**
+     * CTOR
+     * @param p0 Point 3D
+     * @param dir the vector
+     */
     public Ray(Point3D p0, Vector dir) {
         Vector vector = dir.normalized();
         this.p0 = p0;
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -2,10 +2,22 @@ package primitives;
 
 import static primitives.Point3D.ZERO;
 
+/**
+ * A basic object in geometry with direction and size, defined by the end point
+ * (when the starting point - the beginning of the axes).
+ */
 public class Vector {
-
+    /**
+     * the end point of the vector
+     */
     private Point3D head;
 
+    /**
+     * CTOR who gets 3 types of double types
+     * @param x The X-axis component of the end point
+     * @param y The Y-axis component of the end point
+     * @param z The Z-axis component of the end point
+     */
     public Vector(double x, double y, double z) {
         Point3D temp = new Point3D(x, y, z);
         if (temp.equals(ZERO)) {
@@ -14,6 +26,10 @@ public class Vector {
         head = temp;
     }
 
+    /**
+     * CTOR who gets 1 type of the Point3D (the end point of the vector)
+     * @param head
+     */
     public Vector(Point3D head) {
         if (head.equals(ZERO)){
             throw new IllegalArgumentException("head cannot be point(0,0,0,)");
@@ -21,6 +37,12 @@ public class Vector {
         this.head = head;
     }
 
+    /**
+     * CTOR who gets 3 types of Coordinate types
+     * @param x The X-axis component of the end point
+     * @param y The Y-axis component of the end point
+     * @param z The Z-axis component of the end point
+     */
     public Vector(Coordinate x, Coordinate y , Coordinate z) {
         Point3D temp = new Point3D(x,y,z);
         if (head.equals(ZERO)){
@@ -29,18 +51,37 @@ public class Vector {
         head = temp;
     }
 
+    /**
+     * getter for Point3D
+     * @return head
+     */
     public Point3D getHead() {
         return new Point3D(head.getX(), head.getY(), head.getZ());
     }
 
+    /**
+     * A function that performs a vector subtraction between 2 vectors
+     * @param vector A vector that represents the missing vector
+     * @return New vector calculated according to vector subtraction
+     */
     public Vector subtract(Vector vector) {
         return this.head.subtract(vector.head);
     }
 
+    /**
+     * A function that makes a vector connection between 2 vectors
+     * @param vector Vector to be added
+     * @return a new vector
+     */
     public Vector add(Vector vector) {
         return new Vector(this.head.add(vector));
     }
 
+    /**
+     * Vector Multiplier - Scalar
+     * @param scalar The multiplier number
+     * @return New vector after multiplication
+     */
     public Vector scale(double scalar) {
         return new Vector(
                 this.head.getX().coord * scalar,
@@ -48,12 +89,22 @@ public class Vector {
                 this.head.getZ().coord * scalar);
     }
 
+    /**
+     * Scalar product between vectors
+     * @param vector Vector for multiplication
+     * @return The result of the scalar product (double)
+     */
     public double dotProduct(Vector vector) {
         return  this.head.getX().coord * vector.head.getX().coord +
                 this.head.getY().coord * vector.head.getY().coord +
                 this.head.getZ().coord * vector.head.getZ().coord;
     }
 
+    /**
+     * Vector product
+     * @param vector Vector for multiplication
+     * @return a new vector perpendicular to the existing two vectors
+     */
     public Vector crossProduct(Vector vector) {
         return new Vector(
                 this.head.getY().coord * vector.head.getZ().coord - this.head.getZ().coord * vector.head.getY().coord,
@@ -61,19 +112,35 @@ public class Vector {
                 this.head.getX().coord * vector.head.getY().coord - this.head.getY().coord * vector.head.getX().coord);
     }
 
+    /**
+     * Calculate the square of the distance between 2 vectors
+     * @return the square of the distance between 2 vectors
+     */
     public double lengthSquared() {
         return this.head.distanceSquared(new Point3D(0,0,0));
     }
 
+    /**
+     * Calculate the distance between 2 vectors
+     * @return the distance between 2 vectors
+     */
     public double length() {
         return this.head.distance(new Point3D(0,0,0));
     }
 
+    /**
+     * The process of finding the unit vector
+     * @return the vector himself after the normalization operation
+     */
     public Vector normalize() {
         this.head = (scale(1/length()).head);
         return this;
     }
 
+    /**
+     * A normalization operation
+     * @return a new normalized vector in the same direction as the original vector
+     */
     public Vector normalized() {
         Vector vector = new Vector(this.head);
         return new Vector (vector.normalize().head);
