8b55624292c123b379a0426648d579f9f3249ee5
אליסף דימנט <edimant@g.jct.ac.il>
2021-03-21 16:52:18 +0200

    clum

---
+++ Tests/geometries/GeometriesTests.java
@@ -0,0 +1,16 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeometriesTests {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void findIntersections() {
+    }
+}
