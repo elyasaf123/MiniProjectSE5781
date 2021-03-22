9746b6284c187b63cedcef9db0d5ea4f58fddaa9
נתנאל בשן <nbashan@g.jct.ac.il>
2021-03-22 10:30:31 +0200

    Triangle find intersections

--- Tests/geometries/TriangleTests.java
+++ Tests/geometries/TriangleTests.java
@@ -2,7 +2,10 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 /**
@@ -26,6 +29,34 @@ class TriangleTests {
 
     @Test
     void findIntersections() {
+        Triangle tri = new Triangle(new Point3D(1,0,0),new Point3D(1,1,0),new Point3D(2,0,0));
+        // ============ Equivalence Partitions Tests ==============
 
+        // TC01:Ray is intersecting in triangle (1 Points)
+        Ray ray1 = new Ray(new Point3D(1,0,1), new Vector(new Point3D(0.1,0.1,-1)));
+        assertEquals(List.of(new Point3D(1.1,0.1,0)),tri.findIntersections(ray1),"Error - TC01:Ray is intersecting in triangle (1 Points)");
+
+
+        // TC02:Ray is not intersecting with triangle and is parallel to the edge (0 Points)
+        Ray ray2 = new Ray(new Point3D(1.5,-1,1), new Vector(new Point3D(0,0,-1)));
+        assertNull(tri.findIntersections(ray2),"Error - TC02:Ray is not intersecting with triangle and is parallel to the edge (0 Points)");
+
+        // TC03:Ray is not intersecting with triangle and is parallel to the vertx (0 Points)
+        Ray ray3 = new Ray(new Point3D(0.5,-1,1), new Vector(new Point3D(0,0,-1)));
+        assertNull(tri.findIntersections(ray3),"Error - TC03:Ray is not intersecting with triangle and is parallel to the vertx (0 Points)");
+
+
+        // =============== Boundary Values Tests ==================
+        // TC04:Ray is intersecting with triangle on edge (0 Points)
+        Ray ray4 = new Ray(new Point3D(1.5,0,1), new Vector(new Point3D(0,0,-1)));
+        assertNull(tri.findIntersections(ray4),"Error - TC04:Ray is intersecting with triangle on edge (0 Points)");
+
+        // TC05:Ray is intersecting with triangle on vertx (0 Points)
+        Ray ray5 = new Ray(new Point3D(1,0,1), new Vector(new Point3D(0,0,-1)));
+        assertNull(tri.findIntersections(ray5),"Error - TC05:Ray is intersecting with triangle on vertx (0 Points)");
+
+        // TC06:Ray is intersecting with the continuation of the edge (0 Points)
+        Ray ray6 = new Ray(new Point3D(0.5,0,1), new Vector(new Point3D(0,0,-1)));
+        assertNull(tri.findIntersections(ray6),"Error - TC06:Ray is intersecting with the continuation of the edge (0 Points)");
     }
 }
