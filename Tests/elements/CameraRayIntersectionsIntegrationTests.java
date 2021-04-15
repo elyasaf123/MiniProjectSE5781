ade47312c3948041ec85ef7807f278cf62b29aa6
אליסף דימנט <edimant@g.jct.ac.il>
2021-04-16 00:22:30 +0300

    Ex#03 Final commit..

--- Tests/elements/CameraRayIntersectionsIntegrationTests.java
+++ Tests/elements/CameraRayIntersectionsIntegrationTests.java
@@ -1,13 +1,14 @@
 package elements;
+
 import geometries.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import primitives.Point3D;
 import primitives.Vector;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CameraRayIntersectionsIntegrationTests {
-    private void assertCountIntersections(Camera camera, Intersectable geo, int expected){
+    private void assertCountIntersections(Camera camera, Intersectable geo, int expected, String string) {
         int count = 0;
         camera.setViewPlaneSize(3,3).setDistance(1);
         for(int i = 0; i<3; i++){
@@ -16,7 +17,7 @@ public class CameraRayIntersectionsIntegrationTests {
                 count += intersections == null ? 0 : intersections.size();
             }
         }
-        assertEquals("Wrong amount of Intersections", expected,count);
+        assertEquals(expected,count, string);
     }
 
     /**
@@ -27,15 +28,19 @@ public class CameraRayIntersectionsIntegrationTests {
         Camera cam1 = new Camera(Point3D.ZERO, new Vector(0,0,-1), new Vector(0,-1,0));
         Camera cam2 = new Camera(new Point3D(0,0,0.5), new Vector(0,0,-1),new Vector(0,-1,0));
 
-        //TC01: Small sphere 2 points
-        assertCountIntersections(cam1, new Sphere(new Point3D(0,0,-3),1),2);
-        //TC02: Big sphere 18 points
+        //TC01: Small sphere (2 points)
+        assertCountIntersections(cam1, new Sphere(new Point3D(0,0,-3),1),4, "ERROR - TC01: Small sphere (2 points)");
+
+        //TC02: Big sphere (18 points)
         assertCountIntersections(cam2,new Sphere(new Point3D(0,0,-100),90),18);
-        //TC03: Medium sphere 10 points
+
+        //TC03: Medium sphere (10 points)
         assertCountIntersections(cam2,new Sphere(new Point3D(0,0,-2),2),10);
-        //TC04: Inside sphere 9 points
+
+        //TC04: Inside sphere (9 points)
         assertCountIntersections(cam2,new Sphere(new Point3D(0,0,-1),4),9);
-        //TC05: Beyond Sphere 0 points
+
+        //TC05: Beyond Sphere (0 points)
         assertCountIntersections(cam1,new Sphere(new Point3D(0,0,1),0.5),0);
     }
 
@@ -46,13 +51,16 @@ public class CameraRayIntersectionsIntegrationTests {
     public void cameraRayPlaneIntegration(){
         Camera cam = new Camera(Point3D.ZERO, new Vector(0,0,-1), new Vector(0,-1,0));
 
-        //TC01: Plane against camera 9 points
+        //TC01: Plane against camera (9 points)
         assertCountIntersections(cam, new Plane(new Point3D(0,0,-5),new Vector(0,0,1)),9);
-        //TC02: Plane with small angle 9 points
+
+        //TC02: Plane with small angle (9 points)
         assertCountIntersections(cam, new Plane(new Point3D(0,0,-5),new Vector(0,1,2)),9);
-        //TC03: Plane parallel to lower rays 6 points
+
+        //TC03: Plane parallel to lower rays (6 points)
         assertCountIntersections(cam, new Plane(new Point3D(0,0,-5),new Vector(0,1,1)),6);
-        //TC04: Beyond plane 0 points
+
+        //TC04: Beyond plane (0 points)
         assertCountIntersections(cam, new Plane(new Point3D(0,0,5),new Vector(0,1,1)),0);
     }
 
@@ -63,9 +71,10 @@ public class CameraRayIntersectionsIntegrationTests {
     public void cameraRayTriangleIntegration(){
         Camera cam = new Camera(Point3D.ZERO, new Vector(0,0,-1), new Vector(0,-1,0));
 
-        //TC01: small triangle 1 point
+        //TC01: small triangle (1 point)
+
         assertCountIntersections(cam, new Triangle(new Point3D(1,1,-2),new Point3D(-1,1,-2),new Point3D(0,-1,-2)),1);
-        //TC02: medium triangle 2 points
+        //TC02: medium triangle (2 points)
         assertCountIntersections(cam, new Triangle(new Point3D(1,1,-2),new Point3D(-1,1,-2),new Point3D(0,-20,-2)),2);
   }
 }
--- Tests/elements/CameraTest.java
+++ Tests/elements/CameraTest.java
@@ -1,6 +1,9 @@
 package elements;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import primitives.*;
 
 /**
