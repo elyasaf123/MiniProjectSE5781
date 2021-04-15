a43bdcac25fc9395e69d8c6794205a953b8b5bb9
נתנאל בשן <nbashan@g.jct.ac.il>
2021-04-16 00:04:24 +0300

    Triangle find intersections

---
+++ Tests/elements/CameraRayIntersectionsIntegrationTests.java
@@ -0,0 +1,71 @@
+package elements;
+import geometries.*;
+import org.junit.Test;
+import primitives.Point3D;
+import primitives.Vector;
+
+import static org.junit.Assert.*;
+
+public class CameraRayIntersectionsIntegrationTests {
+    private void assertCountIntersections(Camera camera, Intersectable geo, int expected){
+        int count = 0;
+        camera.setViewPlaneSize(3,3).setDistance(1);
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<3; j++) {
+                var intersections = geo.findIntersections(camera.constructRayThroughPixel(3,3,j,i));
+                count += intersections == null ? 0 : intersections.size();
+            }
+        }
+        assertEquals("Wrong amount of Intersections", expected,count);
+    }
+
+    /**
+     * Integration tests of Camera Ray construction with Ray-Sphere Intersections
+     */
+    @Test
+    public void cameraRaySphereIntegration(){
+        Camera cam1 = new Camera(Point3D.ZERO, new Vector(0,0,-1), new Vector(0,-1,0));
+        Camera cam2 = new Camera(new Point3D(0,0,0.5), new Vector(0,0,-1),new Vector(0,-1,0));
+
+        //TC01: Small sphere 2 points
+        assertCountIntersections(cam1, new Sphere(new Point3D(0,0,-3),1),2);
+        //TC02: Big sphere 18 points
+        assertCountIntersections(cam2,new Sphere(new Point3D(0,0,-100),90),18);
+        //TC03: Medium sphere 10 points
+        assertCountIntersections(cam2,new Sphere(new Point3D(0,0,-2),2),10);
+        //TC04: Inside sphere 9 points
+        assertCountIntersections(cam2,new Sphere(new Point3D(0,0,-1),4),9);
+        //TC05: Beyond Sphere 0 points
+        assertCountIntersections(cam1,new Sphere(new Point3D(0,0,1),0.5),0);
+    }
+
+    /**
+     * Integration tests of Camera Ray construction with Ray-Plane Intersections
+     */
+    @Test
+    public void cameraRayPlaneIntegration(){
+        Camera cam = new Camera(Point3D.ZERO, new Vector(0,0,-1), new Vector(0,-1,0));
+
+        //TC01: Plane against camera 9 points
+        assertCountIntersections(cam, new Plane(new Point3D(0,0,-5),new Vector(0,0,1)),9);
+        //TC02: Plane with small angle 9 points
+        assertCountIntersections(cam, new Plane(new Point3D(0,0,-5),new Vector(0,1,2)),9);
+        //TC03: Plane parallel to lower rays 6 points
+        assertCountIntersections(cam, new Plane(new Point3D(0,0,-5),new Vector(0,1,1)),6);
+        //TC04: Beyond plane 0 points
+        assertCountIntersections(cam, new Plane(new Point3D(0,0,5),new Vector(0,1,1)),0);
+    }
+
+    /**
+     * Integration tests of Camera Ray construction with Ray-Triangle Intersections
+     */
+    @Test
+    public void cameraRayTriangleIntegration(){
+        Camera cam = new Camera(Point3D.ZERO, new Vector(0,0,-1), new Vector(0,-1,0));
+
+        //TC01: small triangle 1 point
+        assertCountIntersections(cam, new Triangle(new Point3D(1,1,-2),new Point3D(-1,1,-2),new Point3D(0,-1,-2)),1);
+        //TC02: medium triangle 2 points
+        assertCountIntersections(cam, new Triangle(new Point3D(1,1,-2),new Point3D(-1,1,-2),new Point3D(0,-20,-2)),2);
+  }
+}
---
+++ src/elements/Camera.java
@@ -0,0 +1,93 @@
+package elements;
+
+import primitives.*;
+import static primitives.Util.*;
+
+import java.awt.*;
+
+public class Camera {
+    final Point3D p0;
+    final Vector vTo;
+    final Vector vUp;
+    final Vector vRight;
+
+    private double distance;
+    private double width;
+    private double height;
+
+    public Camera(Point3D p0, Vector vTo, Vector vUp) {
+        this.p0 = p0;
+        this.vTo = vTo.normalized();
+        this.vUp = vUp.normalized();
+
+        if(!isZero(vTo.dotProduct(vUp))) {
+            throw new IllegalArgumentException("vUp is not orthogonal to vTo");
+        }
+
+        vRight = vTo.crossProduct(vUp);
+    }
+
+    /**
+     * borrowing from builder pattern
+     * @param width
+     * @param height
+     * @return
+     */
+    public Camera setViewPlaneSize(double width, double height){
+        this.width = width;
+        this.height = height;
+        return this;
+    }
+
+    /**
+     *
+     * @param distance
+     * @return
+     */
+    public Camera setDistance(double distance){
+        this.distance = distance;
+        return this;
+    }
+
+    public Ray constructRayThroughPixel(int nX,  int nY, int j , int i){
+        Point3D Pc = p0.add(vTo.scale(distance));//the center of the image
+
+        double Ry = height/nY;//height of single pixel
+        double Rx = width/nX;//width of single pixel
+
+        double Yi = ((nY - 1)/2d - i)*Ry;//amount of pixels to move in y axis from pc to i
+        double Xj = (-(nX - 1)/2d + j)*Rx;//amount of pixels  to move in x axis from pc to j
+
+        Point3D Pij = Pc;
+        if(isZero(Yi) && isZero((Xj))){
+            //don't change Pij
+        }
+        else if(isZero(Xj)){
+            Pij = Pc.add(vUp.scale(Yi));//only move on Y axis
+        }
+        else if(isZero(Yi)) {
+            Pij = Pc.add(vRight.scale(Xj));//only move on X axis
+        }
+        else
+            Pij = Pc.add(vRight.scale(Xj).add(vUp.scale(Yi)));//move on both axes
+
+        return new Ray(p0,Pij.subtract(p0));
+    }
+
+    public Point3D getP0() {
+        return p0;
+    }
+
+    public Vector getVTo() {
+        return vTo;
+    }
+
+    public Vector getVUp() {
+        return vUp;
+    }
+
+    public Vector getVRight() {
+        return vRight;
+    }
+
+}
