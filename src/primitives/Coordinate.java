057b1f0786096fdce934bf044a6b7a7e1e268178
אליסף דימנט <edimant@g.jct.ac.il>
2021-04-26 22:57:56 +0300

    EX#05 final commit4

--- src/primitives/Coordinate.java
+++ src/primitives/Coordinate.java
@@ -21,6 +21,7 @@ public final class Coordinate {
      * @param coord coordinate value
      */
     public Coordinate(double coord) {
+
         // if it too close to zero make it zero
         this.coord = alignZero(coord);
     }
